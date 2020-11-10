033c200d8cc27c5996648225391c799f7bf645f8
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-10 22:59:53 +0000

    Update task-3.2.5.js

---
+++ Scripts/task-3.2.5.js
@@ -0,0 +1,10 @@
+var temp = 25
+
+if(temp< 50 ){
+    console.log("Put on your coat")
+}else(temp <30 ){
+    console.log("Put on your coat and hat")
+}
+if(temp <0 ){
+    console.log("Stay inside")
+}
