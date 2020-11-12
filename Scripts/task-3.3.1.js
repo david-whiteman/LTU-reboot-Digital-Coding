777ae595639dcdb3e2a43a1540a03bd2038d2331
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-12 19:48:48 +0000

    Create task-3.3.1.js

---
+++ Scripts/task-3.3.1.js
@@ -0,0 +1,8 @@
+function percentageCalculator ( number , percentage ){
+   
+var percentageOf = ( number * percentage / 100);
+
+return percentageOf ;
+}
+
+document.write ( percentageCalculator(100,25));
