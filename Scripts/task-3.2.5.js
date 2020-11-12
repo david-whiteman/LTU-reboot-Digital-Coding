e50fe43fbbab5dca99828182b1b6b5a1e51a857f
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-12 19:19:54 +0000

    Update task-3.2.5.js

--- Scripts/task-3.2.5.js
+++ Scripts/task-3.2.5.js
@@ -1,12 +1,16 @@
 var temp = 25
 
-if(temp< 50 ){
-    console.log("Put on your coat")
-}
-if(temp <30 ){
-    console.log("Put on your coat and hat")
-}
 if(temp <0 ){
     console.log("Stay inside")
+
+if(temp <30 ){
+        console.log("Put on your coat and hat")}
+
+if(temp< 50 ){
+    console.log("Put on your coat")
+} else{
+    console.log("Just pants and vest is fine")
 }
-//re-order to logical and use else if // 
+
+}
+
