ba50b56cea9f10840913690706f356a132506a2d
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-12 19:27:42 +0000

    Update task-3.2.6.js

--- Scripts/task-3.2.6.js
+++ Scripts/task-3.2.6.js
@@ -1,13 +1,25 @@
-var temp = 25
+var temp = 56
 
 if(temp <0 ){
     console.log("Stay inside")}
 
-if(temp <30 ){
+else if(temp <30 ){
         console.log("Put on your coat and hat")}
 
-if(temp< 50 ){
+else if(temp< 50 ){
     console.log("Put on your coat")
-} else{
+
+
+} else if ( temp > 50 && temp === 55){
+    console.log("It's very hot")
+}
+
+ else if ( temp > 50 || temp === 55){
+    console.log("It's extremely hot")
+}
+
+else{
     console.log("Just pants and vest is fine")
 }
+
+// Logical operators && (AND) || (OR) !(NOT)
