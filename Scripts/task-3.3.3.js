2e13858cdbc3e85fe9e407da6fd4acc3e793c382
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-12 21:02:05 +0000

    Update task-3.3.3.js

--- Scripts/task-3.3.3.js
+++ Scripts/task-3.3.3.js
@@ -1,8 +1,39 @@
 function calculator (number1, operator, number2){
 
+    var result = " ";
 switch (operator){
+    
+   
 
+    case '+' :
+        result = number1 + number2
+        console.log( number1 + " + " + number2 + " = " + result )
+        break;
+
+     case '-' :
+        result = number1 - number2
+        console.log(number1 - number2)
+        break;  
+
+    case '*' :
+        result = number1 * number2
+            console.log(number1 * number2)
+            break;
+
+    case '/' :
+        result = number1 / number2
+        console.log(number1 / number2)
+        break;
+
+    case '%' :
+        result = number1 % number2
+        console.log(number1 % number2)
+        break;
+
+    default :
+        console.log("ERROR")
+        break;
 
 }
 
-}
+}   
