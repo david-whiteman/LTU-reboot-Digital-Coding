752e0ead458ebf96dba9544c8cd7a132dac0e3a3
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-17 20:32:42 +0000

    Update task-3.4.3.js

---
+++ Scripts/task-3.4.3.js
@@ -0,0 +1,8 @@
+var favouriteFoods = [ "Pizza", "Pasta", "Bread", "Chips" ];
+
+for(var index = 0; index < favouriteFoods.length; index++){
+
+    console.log ( favouriteFoods[index] );
+}
+
+document.write(favouriteFoods[1]);
