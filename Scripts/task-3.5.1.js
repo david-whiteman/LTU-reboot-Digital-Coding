373d3c60b89a6fe0616b54abd242a887cefcc921
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-19 20:38:36 +0000

    Update task-3.5.1.js

--- Scripts/task-3.5.1.js
+++ Scripts/task-3.5.1.js
@@ -1,72 +1,72 @@
 let shoppingCart = [
-{​​
-name:"loaf of bread",
-type:"food",
-quantity:1,
-price:.85
-}​​,
-{​​
-name:"multipack beans",
-type:"food",
-quantity:1,
-price:1
-}​​,
-{​​
-name:"mushrooms",
-type:"food",
-quantity:10,
-price:.1
-}​​,
-{​​
-name:"can of beer",
-type:"alcohol",
-quantity:4,
-price:1.1
-}​​,
-{​​
-name:"prosecco",
-type:"alcohol",
-quantity:1,
-price:8.99
-}​​,
-{​​
-name:"steak",
-type:"food",
-quantity:2,
-price:3.99
-}​​,
-{​​
-name:"blue cheese",
-type:"food",
-quantity:1,
-price:2.99
-}​​,
-{​​
-name:"candles",
-type:"home",
-quantity:3,
-price:1.99
-}​​,
-{​​
-name:"cheesecake",
-type:"food",
-quantity:1,
-price:4.99
-}​​,
-{​​
-name:"onions",
-type:"food",
-quantity:3,
-price:.4
-}​​];
+    {
+      name:"loaf of bread",
+      type:"food",
+      quantity:1,
+      price:.85
+    },
+    { 
+      name:"multipack beans",
+      type:"food",
+      quantity:1,
+      price:1
+  },
+  { 
+    name:"mushrooms",
+    type:"food",
+    quantity:10,
+    price:.1
+  },
+  {
+    name:"can of beer",
+    type:"alcohol",
+    quantity:4,
+    price:1.1
+  },
+  {
+    name:"prosecco",
+    type:"alcohol",
+    quantity:1,
+    price:8.99
+  },
+  {
+    name:"steak",
+    type:"food",
+    quantity:2,
+    price:3.99
+  },
+  {
+    name:"blue cheese",
+    type:"food",
+    quantity:1,
+    price:2.99
+  },
+  {
+    name:"candles",
+    type:"home",
+    quantity:3,
+    price:1.99
+  },
+  { 
+    name:"cheesecake",
+    type:"food",
+    quantity:1,
+    price:4.99
+  },
+  { 
+    name:"onions",
+    type:"food",
+    quantity:3,
+    price:.4
+  }];
 
-function shoppingCartTotalPrice ( arr ){ // 1. Create function that takes 1 argument (the array)
+function shoppingCartTotalPrice ( arr ) { // 1. Create function that takes 1 argument (the array)
     var totalPrice = 0; // 2. Create a variable inside the function called 'total price'
 
     // 3. Loop through each item in the array and add the value of the item to the total price, remember to account for the quantity
-    for (var index = 0; index < arr.length; index++){
+    for (var index = 0; index < arr.length; index++ ) {
 
-        totalPrice = totalPrice + ( arr [index].price * arr[index].quantity )
+        totalPrice = totalPrice + ( arr [index].price * arr[index].quantity );
     }
 
     // 4. return the totalPrice variable 
@@ -74,4 +74,4 @@ function shoppingCartTotalPrice ( arr ){ // 1. Create function that takes 1 argu
     return totalPrice.toFixed(2);
 }
 
-console.log(ShoppingCartTotalPrice (shoppingCart));
+console.log(shoppingCartTotalPrice (shoppingCart));
