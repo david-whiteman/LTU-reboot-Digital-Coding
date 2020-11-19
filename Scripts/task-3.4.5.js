41a71dc56e0e6f200e52e0090cfaeb07268ae986
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-19 19:25:25 +0000

    Create task-3.4.5.js

---
+++ Scripts/task-3.4.5.js
@@ -0,0 +1,11 @@
+var recipe = {
+    recipeTitle: "Cup of Coffee",
+    servings: 1,
+    ingredients: ["Coffee Beans", "Water", "Milk"],
+    directions: "Boil Water. Add coffee beans to cup. Add boiling water and milk. Serve",
+
+
+letsCook: function(){
+    document.write("I'm hungry! Let's cook " + recipe.recipeTitle)
+}
+}
