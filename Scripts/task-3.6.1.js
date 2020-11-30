615abcdbb339870003c2ed23718b123ba24d084d
david-whiteman <72816394+david-whiteman@users.noreply.github.com>
2020-11-30 21:00:15 +0000

    Update task-3.6.1.js

---
+++ Scripts/task-3.6.1.js
@@ -0,0 +1,8 @@
+var catImage = document.getElementById('cat');
+
+catImage.src = 'https://media.gq.com/photos/56e71c0b14cbe0637b261d7f/16:9/w_2560%2Cc_limit/horseinsuit2.jpg'
+
+var pageBody = document.getElementsByTagName('p')[0]
+pageBody.style.backgroundColor = 'pink';
+pageBody.style.paddingTop = '10px';
+
